40b6b0c065fd743c3ae403791b43a3cd6a4bd2e9
J. Eduardo V. Morango <edvmorango@gmail.com>
2018-09-15 18:33:30 -0300

    tests: ChiSquared

--- src/ChiSquared.hs
+++ src/ChiSquared.hs
@@ -11,16 +11,3 @@ frequency t = (els, diff)
   where
     els = foldr (\(e, _) acc -> acc + e) 0 t
     diff = (-) 1 $ foldr (+) 0 (chiSquaredExecutor t)
-
-test :: [(Float, Float)]
-test =
-  [ (301, 302)
-  , (176, 175)
-  , (125, 126)
-  , (97, 96)
-  , (79, 80)
-  , (67, 66)
-  , (58, 59)
-  , (51, 50)
-  , (46, 46)
-  ]
