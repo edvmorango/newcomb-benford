d62627e0dd3a2de88e1f75281a24e5d29bd94f08
J. Eduardo V. Morango <edvmorango@gmail.com>
2018-09-16 18:58:14 -0300

    feature: Semigroup and Monoid for Items

--- src/NBData.hs
+++ src/NBData.hs
@@ -1,20 +1,46 @@
 module NBData where
 
+import Data.Monoid
+import Data.Semigroup
+
 type Tag = String
+
+data Prefix
+  = P1
+  | P2
+  | P3
+  | P4
+  | P5
+  | P6
+  | P7
+  | P8
+  | P9
+  | P0
+  deriving (Eq, Show)
 
 data Item = Item
   { tag :: Tag
   , code :: Integer
-  }
+  } deriving (Eq, Show)
 
-data ItemGroup = ItemGroup
-  { prefix :: Int
-  , items :: [Item]
-  , total :: Integer
-  }
+data ItemGroup
+  = EmptyGroup
+  | ItemGroup Prefix
+              [Item]
+              Integer
+  deriving (Eq, Show)
 
 data Batch = Batch
-  { tag :: Tag
-  , groups :: [ItemBatch]
-  , total :: Integer
-  }
+  { btag :: Tag
+  , groups :: [ItemGroup]
+  , btotal :: Integer
+  } deriving (Eq, Show)
+
+instance Semigroup ItemGroup where
+  EmptyGroup <> g = g
+  g <> EmptyGroup = g
+  (ItemGroup p es t) <> (ItemGroup _ es' t') = ItemGroup p (es <> es') (t + t')
+
+instance Monoid ItemGroup where
+  mempty = EmptyGroup
+  mappend i i' = i <> i'
