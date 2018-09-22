a0ed3d4b384ddaa4d08930fa2ec78580ad5d40d2
J. Eduardo V. Morango <edvmorango@gmail.com>
2018-09-22 15:52:49 -0300

    feature: semigroup instances

--- src/NBLib.hs
+++ src/NBLib.hs
@@ -21,12 +21,22 @@ type NBItemGroup = ItemGroup Integer Integer [Item]
 data Batch a b c where
   Batch :: Tag -> Integer -> [NBItemGroup] -> Batch Tag Integer [NBItemGroup]
 
+------ ItemGroup Instances
 instance Show (ItemGroup a b c) where
   show (ItemGroup a b c) =
     "ItemGroup -> Prefix [" ++
     (show a) ++ "] Items Length [" ++ (show b) ++ "] Items " ++ (show c)
 
+instance Semigroup (ItemGroup a b c) where
+  (ItemGroup p l is) <> (ItemGroup _ l' is') = ItemGroup p (l + l') (is ++ is')
+
+------ Batch Instances
 instance Show (Batch a b c) where
   show (Batch a b c) =
     "Batch -> Items Tag [" ++
     a ++ "] Length [" ++ (show b) ++ "] ItemsGroups " ++ (show c)
+
+instance Semigroup (Batch a b c) where
+  (Batch t l igs) <> (Batch _ l' igs') = Batch t (l + l') merge
+    where
+      merge = fmap (\(a, b) -> a <> b) (zip igs igs')
