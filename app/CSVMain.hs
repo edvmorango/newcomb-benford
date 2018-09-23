38ed9533cba5b6859f5fa1307eb876bfccab7101
J. Eduardo V. Morango <edvmorango@gmail.com>
2018-09-23 16:19:13 -0300

    feature: reading csv

--- app/CSVMain.hs
+++ app/CSVMain.hs
@@ -4,11 +4,40 @@
 
 module CSVMain where
 
+import Control.Exception
 import qualified Data.ByteString.Lazy as BL
 import Data.Csv
 import qualified Data.Vector as V
 import NBLib
 
+data OrderCSV = OrderCSV
+  { amount :: !Float
+  } deriving (Show)
+
+instance FromNamedRecord OrderCSV where
+  parseNamedRecord r = OrderCSV <$> r .: "amount"
+
+type OrderV = (Header, V.Vector OrderCSV)
+
+csvReader :: IO (Either String OrderV)
+csvReader =
+  BL.readFile "dataset/orders.csv" >>= (\csv -> return (decodeByName csv)) --do
+
+data CSVException =
+  CSVException String
+  deriving (Show)
+
+instance Exception CSVException
+
 csvMain :: IO ()
 csvMain = do
-  putStrLn "123123"
+  ordersV <- csvReader
+  orders <-
+    case ordersV of
+      Left s -> throwIO $ CSVException s
+      Right (_, v) -> return $ V.toList v
+  putStrLn $ show (length orders)
+-- csv <- BL.readFile "dataset/test.csv"
+--case (decodeByName csv) of
+--    Left err -> putStrLn $ "Error: \n" ++ err
+--    Right (_, v) -> V.forM_ v $ \c -> putStrLn $ "Value:" ++ (show (amount c))
